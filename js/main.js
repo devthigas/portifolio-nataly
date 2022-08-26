b2b0c05bea5d42355546efafd282727a6b61ca57
devthigas <thi.deusefiel@gmail.com>
2022-08-26 13:13:43 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+new TypeIt("#animate", {
+    strings: " Bem-vindo ao meu portifólio! Desenvolvo aplicações utilizando HTML, CSS e JavaScript.",
+    speed: 100,
+    waitUntilVisible: true,
+}).go();
+
+AOS.init()
