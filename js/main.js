a1fd7d39c5e2da4c779723413cf355662219df3e
devthigas <thi.deusefiel@gmail.com>
2022-08-31 11:27:07 -0300

    revisão

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+
+
+// Fiz a inicialização das bibliotecas e passei os parametros de animação 
+
+// pegar elemento pelo id passar a string na variavel, passar velocidade da animação. e passar valor para deixar visivel ou não
+// neste caso ficará visivel 
 new TypeIt("#animate", {
     strings: " Bem-vindo ao meu portifólio! Desenvolvo aplicações utilizando HTML, CSS e JavaScript.",
     speed: 100,
@@ -5,3 +11,7 @@ new TypeIt("#animate", {
 }).go();
 
 AOS.init()
+
+function btnMsg(){
+    alert("Formulário em Construção!!")
+}
